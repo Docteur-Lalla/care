eaa2468a5129e1753530412302bf62308faea24a
Camille Le Bon <camille.lebon35000@gmail.com>
2021-07-06 23:39:54 +0200

    Add the Parser module and a function to parse numbers

---
+++ src/Care/Parser.hs
@@ -0,0 +1,31 @@
+module Care.Parser (number) where
+  import Control.Applicative ((<$>), (<*>))
+  import Data.Maybe (fromMaybe)
+
+  import Text.Parsec
+  import Text.Parsec.Char
+  import Text.Parsec.String
+
+  import Care.Value
+
+  integerPart :: Parser String
+  integerPart = many1 digit
+
+  decimalPart :: Parser String
+  decimalPart = (:) <$> (char '.') <*> (integerPart)
+
+  sign :: Parser Char
+  sign = oneOf "-+"
+
+  -- | Parse a number.
+  number :: Parser CareValue
+  number = do
+    s <- fromMaybe '+' <$> optionMaybe sign
+    int <- integerPart
+    opt <- optionMaybe decimalPart
+    let sint = s : int
+    case opt of
+      Just dec -> readNum careFloat $ sint ++ dec
+      Nothing -> readNum careInteger $ sint
+    where readNum :: Read a => (a -> CareValue) -> String -> Parser CareValue
+          readNum ctor = return . ctor . read
